d6830c90aaeb5d5b441840d8011409969238cb03
Pongpan Poonkhuntod <go16022538@hotmail.com>
2020-10-12 10:26:54 +0700

    pom

--- src/main/java/com/dropbox/payment/PaymentApplication.java
+++ src/main/java/com/dropbox/payment/PaymentApplication.java
@@ -2,6 +2,14 @@ package com.dropbox.payment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.nio.charset.Charset;
 
 @SpringBootApplication
 public class PaymentApplication {
@@ -10,4 +18,12 @@ public class PaymentApplication {
 		SpringApplication.run(PaymentApplication.class, args);
 	}
 
+	@Bean
+	@Scope(value = WebApplicationContext.SCOPE_REQUEST, proxyMode = ScopedProxyMode.TARGET_CLASS)
+	public RestTemplate restTemplate(){
+		RestTemplate restTemplate = new RestTemplate();
+		restTemplate.getMessageConverters()
+				.add(0, new StringHttpMessageConverter(Charset.forName("UTF-8")));
+		return restTemplate;
+	}
 }
